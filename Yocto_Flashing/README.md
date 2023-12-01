26c6d2b7ad1c5eb5ce7049bbc2829580cdde833c
Khaled El-Sayed <t0ti20@icloud.com>
2023-12-01 17:35:07 +0200

    Update Yocto Flasing Script

---
+++ Yocto_Flashing/README.md
@@ -0,0 +1,51 @@
+# Yocto Flashing Script
+
+This Bash script provides a user-friendly interface for flashing, mounting, and configuring a Yocto image on a Raspberry Pi.
+
+## Script Variables
+
+- **WIC_Image:** Default path to the Yocto image file.
+
+## Usage
+
+1. Run the script: `./Yocto_Flashing.sh`
+2. Follow the on-screen prompts to perform actions such as flashing, mounting, and configuring.
+
+## Actions
+
+### 1. Flashing Image
+
+- Prompts for the Yocto image file location.
+- Allows editing the image file location.
+- Prompts for the target device to flash.
+- Flashes the Yocto image onto the specified device.
+
+### 2. Mounting Device
+
+- Prompts for the target device to mount.
+- Creates mount points for boot and root partitions.
+- Mounts the specified device partitions.
+
+### 3. Configuring Device
+
+- Configures the cmdline.txt and config.txt files on the boot partition.
+
+### 4. Exit
+
+- Exits the script.
+
+## Notes
+
+- Ensure you have the necessary permissions to perform the specified actions.
+- Exercise caution when modifying system files.
+- Make sure to review and understand the script before usage.
+
+## Author
+
+- Author: [Your Name]
+- Email: [Your Email]
+
+Feel free to contribute or report issues on [GitHub Repository](link-to-your-repo).
+
+---
+
---
+++ Yocto_Flashing/Yocto_Flashing.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+############################################
+#------------ Script Variables -------------
+############################################
+WIC_Image="/home/t0ti20/Shared/rpi-test-image-raspberrypi4-64-20231201135305.rootfs.wic.bz2"
+
+############################################
+#------------ Flashing Image ---------------
+############################################
+Do_Flash()
+{
+     while true; do
+          echo "WIC Image: ${WIC_Image}"
+          read -p "Do you want to edit the above information? (y/n): " Confirm
+          case $Confirm in
+          "n" | "N")
+               break
+               ;;
+          "y" | "Y")
+               read -p "Enter WIC Image Location: " WIC_Image
+               ;;
+          *)
+               echo "Invalid Option. Please enter 'y' for yes or 'n' for no."
+               ;;
+          esac
+     done
+
+     echo "==================================="
+     lsblk
+     echo "==================================="
+     read -p "Please Choose Device To Flash: " Device
+     bzcat "${WIC_Image}" | sudo dd of="/dev/${Device}" status=progress
+     echo "Done Flashing."
+
+     while true; do
+          read -p "Do you want to mount? (y/n): " Confirm
+          case $Confirm in
+          "n" | "N")
+               break
+               ;;
+          "y" | "Y")
+               Do_Mount
+               ;;
+          *)
+               echo "Invalid Option. Please enter 'y' for yes or 'n' for no."
+               ;;
+          esac
+     done
+
+     while true; do
+          read -p "Do you want to configure? (y/n): " Confirm
+          case $Confirm in
+          "n" | "N")
+               break
+               ;;
+          "y" | "Y")
+               Do_Configure
+               ;;
+          *)
+               echo "Invalid Option. Please enter 'y' for yes or 'n' for no."
+               ;;
+          esac
+     done
+}
+
+############################################
+#----------- Mounting Device ---------------
+############################################
+Do_Mount()
+{
+     echo "==================================="
+     lsblk
+     echo "==================================="
+     read -p "Please Choose Device To Mount: " Device
+     sudo mkdir -p /Media/USB/boot /Media/USB/root
+     sudo mount -t vfat "/dev/${Device}1" /Media/USB/boot -o rw
+     sudo mount "/dev/${Device}2" /Media/USB/root -o rw
+     echo "==================================="
+     lsblk
+     echo "==================================="
+     echo "Done Mounting."
+}
+
+############################################
+#----------- Configuring Device ------------
+############################################
+Do_Configure()
+{
+     sudo bash -c 'echo "dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 console=ttyS0 root=/dev/sda2 rootfstype=ext4 rootwait" > /Media/USB/boot/cmdline.txt'
+     sudo bash -c 'echo -e "enable_uart=1\narm_64bit=1" >> /Media/USB/boot/config.txt'
+     echo "Done Configuring."
+}
+
+############################################
+#----------- Main Application --------------
+############################################
+while true; do
+     echo "==================================="
+     echo "1) Flashing."
+     echo "2) Mounting."
+     echo "3) Configure."
+     echo "4) Exit."
+     read -p "- What You Want To Do? " Confirm
+     case $Confirm in
+     "1")
+          Do_Flash
+          ;;
+     "2")
+          Do_Mount
+          ;;
+     "3")
+          Do_Configure
+          ;;
+     "4")
+          break
+          ;;
+     *)
+          echo "Invalid Option!"
+          ;;
+     esac
+done
+
+######################################################################
+# *  END OF FILE:  Yocto Flashing
+######################################################################
